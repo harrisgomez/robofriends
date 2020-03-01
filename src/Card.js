fd21aa431bdbe820c3a5c197a9719fe3c4ded2ba
Harris G <node.harris@gmail.com>
2020-03-01 01:03:28 -0500

    Created static api

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ id, name, email }) => {
     return (
-        <div>
-            <img src="https://robohash.org/test" alt=""/>
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
+            <img src={`https://robohash.org/${id}?200x200`} alt=""/>
             <div>
-                <h2>Name</h2>
-                <p>Email</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
---
+++ src/robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Jan Doe',
+        username: 'Jan',
+        email: 'Jan@one.com'
+    },
+    {
+        id: 2,
+        name: 'Feb Doe',
+        username: 'Feb',
+        email: 'Feb@two.com'
+    },
+    {
+        id: 3,
+        name: 'Jan Doe',
+        username: 'Jan',
+        email: 'Mar@three.com'
+    },
+    {
+        id: 4,
+        name: 'Jan Doe',
+        username: 'Jan',
+        email: 'Apr@four.com'
+    },
+    {
+        id: 5,
+        name: 'Jan Doe',
+        username: 'Jan',
+        email: 'May@five.com'
+    },
+    {
+        id: 6,
+        name: 'Jan Doe',
+        username: 'Jan',
+        email: 'Jun@six.com'
+    },
+    {
+        id: 7,
+        name: 'Jan Doe',
+        username: 'Jan',
+        email: 'Jul@seven.com'
+    },
+    {
+        id: 8,
+        name: 'Jan Doe',
+        username: 'Jan',
+        email: 'Aug@oeightne.com'
+    },
+    {
+        id: 9,
+        name: 'Jan Doe',
+        username: 'Jan',
+        email: 'Sep@nine.com'
+    },
+    {
+        id: 10,
+        name: 'Jan Doe',
+        username: 'Jan',
+        email: 'Oct@ten.com'
+    },
+    {
+        id: 11,
+        name: 'Jan Doe',
+        username: 'Jan',
+        email: 'Nov@eleven.com'
+    },
+    {
+        id: 12,
+        name: 'Jan Doe',
+        username: 'Jan',
+        email: 'Dec@twelve.com'
+    }
+];
