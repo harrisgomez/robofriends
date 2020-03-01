fa7a4faef5922ce60e5fedcbd91085c2a79b3a1b
Harris G <node.harris@gmail.com>
2020-02-29 23:10:27 -0500

    Added image source

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img src="https://robohash.org/test" alt="robot"/>
+            <div>
+                <h2>Name</h2>
+                <p>Email</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
